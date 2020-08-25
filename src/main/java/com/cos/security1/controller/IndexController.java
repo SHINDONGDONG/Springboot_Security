1b8d7802eefb4daf564b71101f2628b6f14f669a
cos <homerjay@naver.com>
2020-08-25 09:59:40 +0900

    cru

--- src/main/java/com/cos/security1/controller/IndexController.java
+++ src/main/java/com/cos/security1/controller/IndexController.java
@@ -1,6 +1,9 @@
 package com.cos.security1.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -61,6 +64,19 @@ public class IndexController {
 			user.setPassword(encPassword);
 			userRepository.save(user); //회원가입 비밀번호 1234 => 시큐리티로 로그인 불가능 	ㅍ패스워드 암호화가 안되었기 때문에 
 		return "redirect:/loginForm";
+	}
+		
+	@Secured("ROLE_ADMIN")  //특정메소드에 권한을 주고싶을때
+	@GetMapping("/info")
+	public @ResponseBody String info() {
+		return "개인정보";
+	}
+
+	//@PostAuthorize
+	@PreAuthorize("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+	@GetMapping("/data")
+	public @ResponseBody String data() {
+		return "데이터정보";
 	}
 
 }
